7f916652303d23f4c2c60699edb7201efe0abfa3
SEOP <sshwang12345@gmail.com>
2021-05-17 16:33:47 +0900

    Created Momentum

---
+++ 2_momentum_practice/clock.js
@@ -0,0 +1,22 @@
+const clockContainer = document.querySelector(".js-clock"),
+  clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+  const date = new Date();
+  const minutes = date.getMinutes();
+  const hours = date.getHours();
+  const seconds = date.getSeconds();
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  }:${
+    seconds < 10 ? `0${seconds}` : seconds
+    // ternary operator(삼항연산자) / 작은 if
+  }`;
+}
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
+
+init();
