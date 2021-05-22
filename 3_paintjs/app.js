857266bf02929c40025f3872be889685c88b86eb
SEOP <sshwang12345@gmail.com>
2021-05-22 19:28:17 +0900

    updated paintjs!

--- 3_paintjs/app.js
+++ 3_paintjs/app.js
@@ -1,8 +1,10 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = 700;
+canvas.height = 700;
 
+ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let painting = false;
