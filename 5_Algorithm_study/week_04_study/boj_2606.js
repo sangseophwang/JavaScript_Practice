ed2e7c639e29c83439a1393974f3d3b6583a7028
SEOP <sshwang12345@gmail.com>
2021-08-09 23:29:52 +0900

    백준 문제풀기 🚀

---
+++ 5_Algorithm_study/week_04_study/boj_2606.js
@@ -0,0 +1,38 @@
+const readline = require('readline'); 
+const rl = readline.createInterface({ 
+  input: process.stdin, 
+  output: process.stdout, }); 
+
+// 코드 시작
+const solution = (input) => { 
+  const n = +input.shift(); 
+  const m = +input.shift(); 
+  const graph = [...new Array(n + 1)].map(() => []); 
+  const visited = [...new Array(n + 1)].fill(0); 
+  let cnt = 0; 
+
+  for (const edge of input) { 
+    const [start, destination] = edge.split(' ').map(e => +e); 
+    graph[start].push(destination); 
+    graph[destination].push(start); 
+  } 
+  visited[1] = 1; 
+  const dfs = (start) => { 
+    for (const destination of graph[start]) {
+      if (!visited[destination]) { 
+        visited[destination] = 1; 
+        cnt++; 
+        dfs(destination); 
+      } } }; 
+
+    dfs(1); 
+    console.log(cnt); }; 
+
+    const input = []; 
+    rl.on('line', function (line) {
+      input.push(line); 
+    }).on('close', function () {
+      solution(input); 
+      process.exit(); 
+    });
+
