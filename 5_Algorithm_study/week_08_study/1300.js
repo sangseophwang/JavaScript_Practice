2b05b3b67bf6cff7a8b98e13e0f1ba8ba8dd4baa
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2021-10-07 14:01:26 +0900

    Algorithm Study_Week 8

--- 5_Algorithm_study/week_08_study/1300.js
+++ 5_Algorithm_study/week_08_study/1300.js
@@ -1,10 +1,25 @@
-const readline = require("readline");
+const [N, k] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((num) => Number(num));
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+let left = 1;
+let right = k;
+let answer = 0;
 
-let input = [];
+while (left <= right) {
+  let count = 0;
+  let mid = Math.floor((left + right) / 2); // 중간값
+  for (let i = 1; i <= N; i++) {
+    count += Math.min(Math.floor(mid / i), N); // 각 행에서 mid보다 작은 값이 몇 개인지
+  }
+  if (count < k) left = mid + 1;
+  else {
+    answer = mid;
+    right = mid - 1;
+  }
+}
 
-rl.on("line", (line) => input.push(line)).on("close", () => {});
+console.log(answer);
