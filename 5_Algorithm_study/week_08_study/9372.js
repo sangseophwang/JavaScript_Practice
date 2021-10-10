23f17250aa257398d0cbb6dfcd5d290c91d45892
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2021-10-10 21:38:46 +0900

    Algorithm Study_Week 8

--- 5_Algorithm_study/week_08_study/9372.js
+++ 5_Algorithm_study/week_08_study/9372.js
@@ -7,4 +7,16 @@ const rl = readline.createInterface({
 
 let input = [];
 
-rl.on("line", (line) => input.push(line)).on("close", () => {});
+rl.on("line", (line) => input.push(line)).on("close", () => {
+  const T = Number(input[0]);
+  let line = 1;
+  for (let i = 0; i < T; i++) {
+    let [n, m] = input[line].split(" ").map((x) => +x);
+    console.log(n - 1);
+    line = line + Number(input[line].split(" ")[1]) + 1;
+  }
+});
+
+// 1 2 3 - 비행기 2대
+// 1 2 3 4 5 - 비행기 4대
+// 노드 개수 - 1 (2, 4)
