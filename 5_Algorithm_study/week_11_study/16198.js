89103c8db8884ccff30c1b47b86f9ba18cea3a49
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2021-11-11 23:11:30 +0900

    Update 16198.js

--- 5_Algorithm_study/week_11_study/16198.js
+++ 5_Algorithm_study/week_11_study/16198.js
@@ -10,5 +10,14 @@ let input = [];
 rl.on("line", (line) => input.push(line)).on("close", () => {
   const N = +input.shift();
   const weight = input[0].split(" ").map((e) => +e);
-  console.log(weight);
+  let answer = 0;
+  function energy(weight, sum) {
+    let temp = [...weight];
+    if (weight.length === 2) answer = Math.max(answer, sum);
+    for (let i = 1; i < temp.length - 1; i++) {
+      let rest = [...temp.slice(0, i), ...temp.slice(i + 1)];
+      energy(rest, sum + temp[i - 1] * temp[i + 1]);
+    }
+  }
+  energy;
 });
