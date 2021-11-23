9dc30e431778990bd4166bdb561c22347ab8195b
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2021-11-23 22:54:27 +0900

    Update 11497.js

--- 5_Algorithm_study/week_11_study/11497.js
+++ 5_Algorithm_study/week_11_study/11497.js
@@ -9,11 +9,27 @@ let input = [];
 
 rl.on("line", (line) => input.push(line)).on("close", () => {
   input.shift();
-  let array = [];
-  let result = 0;
+  let array;
+  let result;
 
-  input.forEach((i, index) => {
-    if (index % 2 === 1) {
+  input.forEach((i, idx) => {
+    result = 0;
+    array = [];
+    if (idx % 2 === 1) {
+      let arr = i.split(" ");
+      arr.sort((a, b) => a - b);
+      arr.forEach((j, idx2) => {
+        if (idx2 % 2 === 0) array.push(j);
+        else array.unshift(j);
+      });
+      array.forEach((i, idx) => {
+        let data;
+        if (idx == array.length - 1) data = Math.abs(i - array[0]);
+        else data = Math.abs(i - array[idx + 1]);
+
+        if (result < data) result = data;
+      });
+      console.log(result);
     }
   });
 });
