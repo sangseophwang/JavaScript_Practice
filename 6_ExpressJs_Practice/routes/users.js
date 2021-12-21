ab64d93c79e26f348c4d48d824cc530d070b3896
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2021-12-21 22:13:58 +0900

    Feat: Parsing Form

--- 6_ExpressJs_Practice/routes/users.js
+++ 6_ExpressJs_Practice/routes/users.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
+router.use(logger);
+
 router.get("/", (req, res) => {
   res.send("User List");
 });
 
 router.get("/new", (req, res) => {
-  res.send("New User Form");
+  res.render("users/new");
 });
 
 router.post("/", (req, res) => {
-  res.send("Create User");
+  const isValid = true;
+  if (isValid) {
+    users.push({ firstName: req.body.firstName });
+    res.redirect(`/users/${users.length - 1}`);
+  } else {
+    console.log("Error");
+    res.render("users/new", { firstName: req.body.firstName });
+  }
+  res.send("hi");
 });
 
 router
@@ -32,5 +42,10 @@ router.param("id", (req, res, next, id) => {
   req.user = users[id];
   next();
 });
+
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
 
 module.exports = router;
--- 6_ExpressJs_Practice/server.js
+++ 6_ExpressJs_Practice/server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.render("index", { text: "World!" });
-});
+app.set("view engine", "ejs");
 
 const userRouter = require("./routes/users");
 
