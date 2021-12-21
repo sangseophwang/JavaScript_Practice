d01b6fb366b140d777f83ab001735c15b459d771
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2021-12-21 14:01:08 +0900

    Feat: ejs 설치, 텍스트 전달

--- 6_ExpressJs_Practice/server.js
+++ 6_ExpressJs_Practice/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.json({ message: "Hello world!" });
+  res.render("index", { text: "World!" });
 });
 
 app.listen(3001);
