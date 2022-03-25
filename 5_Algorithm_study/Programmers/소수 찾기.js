2352e4e5e756cad999da6d050cfff4f5d8e07a48
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-03-26 00:02:16 +0900

    Create 소수 찾기.js

---
+++ 5_Algorithm_study/Programmers/소수 찾기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  const prime = [false, false, ...Array(n - 1).fill(true)];
+
+  for (let i = 2; i * i <= n; i++) {
+    if (prime[i]) {
+      for (let j = i * 2; j <= n; j += i) {
+        prime[j] = false;
+      }
+    }
+  }
+  return prime.filter(Boolean).length;
+}
