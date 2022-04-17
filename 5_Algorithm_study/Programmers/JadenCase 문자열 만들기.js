30037bbbb66df3d5ea8dea5040592b918e9104bf
[2기-2반] 황상섭 <sshwang12345@gmail.com>
2022-04-17 13:04:37 +0900

    Create JadenCase 문자열 만들기.js

---
+++ 5_Algorithm_study/Programmers/JadenCase 문자열 만들기.js
@@ -0,0 +1,7 @@
+function solution(s) {
+  const words = s.toLowerCase().split(" ");
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+  }
+  return words.join(" ");
+}
